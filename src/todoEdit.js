d260334bb989910697b9807b90f10396be0cea22
alihaider1145 <alihaider15112006@gmail.com>
2025-12-29 09:58:10 +0500

    Add delete todo feature

--- src/todoEdit.js
+++ src/todoEdit.js
@@ -3,19 +3,25 @@ import globalState from "./state";
 
 function editTodoHandler(e) {
     const todoId = e.target.closest("li").id;
-    showTodoForm();
 
-    const todo = globalState.getState().todoListItems.find(todo => todo.id === todoId);
-    document.querySelector("#todo-title").value = todo.title;
-    document.querySelector("#todo-desc").value = todo.desc;
-    document.querySelector("#todo-due-date").value = todo.dueDate.toISOString().split("T")[0];
-    document.querySelector("#todo-priority").value = todo.priority;
+    if(e.target.classList.contains("todo-del-btn"))
+    {
+        globalState.setState({todoListItems: globalState.getState().todoListItems.filter(todo => todo.id !== todoId)});
+    }
+    if(e.target.classList.contains("todo-item"))
+    {
+        showTodoForm();
 
-    document.querySelector(".todo-submit-btn").textContent = "Update";
-    globalState.setState({currentTodoId: `${todoId}`});
-    globalState.setState({submitAction: "update"});
+        const todo = globalState.getState().todoListItems.find(todo => todo.id === todoId);
+        document.querySelector("#todo-title").value = todo.title;
+        document.querySelector("#todo-desc").value = todo.desc;
+        document.querySelector("#todo-due-date").value = todo.dueDate.toISOString().split("T")[0];
+        document.querySelector("#todo-priority").value = todo.priority;
 
-    showTodoForm();
+        document.querySelector(".todo-submit-btn").textContent = "Update";
+        globalState.setState({currentTodoId: `${todoId}`});
+        globalState.setState({submitAction: "update"});
+    }
 }
 
 export { editTodoHandler };
